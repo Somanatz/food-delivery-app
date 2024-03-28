04536d2899ea57953c98c301f90c0ebd0ef6c36d
Obili Narugari Somanath Reddy <121339264+Somanatz@users.noreply.github.com>
2024-03-28 13:29:47 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,19 @@
+// database.js
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'postgres',
+    host: 'localhost',
+});
+
+// Test the database connection
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connection has been established successfully.');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
+
+module.exports = sequelize;
